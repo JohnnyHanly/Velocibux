70e6df9ae8b137a74263c720479bfc98265d45ea
Johnny Hanly <johnnymhanly@gmail.com>
2017-11-10 20:18:33 -0800

    restrctured xml files, working on mainactivity

--- app/src/main/java/teamvelociraptor/assignment4/Chat.java
+++ app/src/main/java/teamvelociraptor/assignment4/Chat.java
@@ -1,20 +1,10 @@
 package teamvelociraptor.assignment4;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
 
-import java.util.HashMap;
-import java.util.Map;
+
 public class Chat extends AppCompatActivity{
 
-    EditText chatBox;
-    ScrollView scrollView;
-    ImageView sendButton;
-    LinearLayout layout;
-    RelativeLayout relativeLayout;
+
 
 
 
--- app/src/main/java/teamvelociraptor/assignment4/MainActivity.java
+++ app/src/main/java/teamvelociraptor/assignment4/MainActivity.java
@@ -1,20 +1,55 @@
 package teamvelociraptor.assignment4;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth Auth;
+    private FirebaseUser User;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private static DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference();
+    private FirebaseAuth.AuthStateListener AuthListener;
+    private GoogleApiClient GoogleApiClient;
+    private FloatingActionButton sendButton;
+    RelativeLayout message_chat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Auth = FirebaseAuth.getInstance();
+        User= Auth.getCurrentUser();
+
+        sendButton= findViewById(R.id.sendButton);
 
 
 
+
+    }
+
+
+
+
+
+
+    public static void sendMessage(Message message){
 
 
 
