55ab84fabcfc1362cf7de046744d45465178e17c
Johnny Hanly <johnnymhanly@gmail.com>
2017-11-10 18:34:37 -0800

    working on chat

--- app/src/main/java/teamvelociraptor/assignment4/Chat.java
+++ app/src/main/java/teamvelociraptor/assignment4/Chat.java
@@ -1,10 +1,20 @@
 package teamvelociraptor.assignment4;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
-/**
- * Created by Johnny Hanly on 11/10/2017.
- */
+import java.util.HashMap;
+import java.util.Map;
+public class Chat extends AppCompatActivity{
 
-public class Chat {
+    EditText chatBox;
+    ScrollView scrollView;
+    ImageView sendButton;
+    LinearLayout layout;
+    RelativeLayout relativeLayout;
 
 
 
