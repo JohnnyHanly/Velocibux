21fbea599b522d3733dd5db196b384481d8f7dcb
Preston <psegura1@mail.sfsu.edu>
2017-11-14 20:34:24 -0800

    Deleted sing in activity, working through main activity

--- app/src/main/java/teamvelociraptor/assignment4/MainActivity.java
+++ app/src/main/java/teamvelociraptor/assignment4/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    protected void onStart() {
+
+        super.onStart();
+    }
 
 }
