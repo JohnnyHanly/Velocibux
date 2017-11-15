5908191933ddfcb9c6b7ee7ac55858266f8e6458
Preston <psegura1@mail.sfsu.edu>
2017-11-15 13:40:33 -0800

    Fixed Sign in button

--- app/src/main/java/teamvelociraptor/assignment4/MainActivity.java
+++ app/src/main/java/teamvelociraptor/assignment4/MainActivity.java
@@ -13,13 +13,14 @@ import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInApi;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener{
 
     private LinearLayout Prof_Section;
     private Button SignOut;
-    private Button SignIn;
+    private SignInButton SignIn;
     private TextView Name,Email;
     private ImageView Prof_Pic;
     private GoogleApiClient googleApiClient;
@@ -31,20 +32,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Prof_Section = (LinearLayout)findViewById(R.id.prof_section);
         SignOut = (Button)findViewById(R.id.bn_logout);
-        SignIn = (Button)findViewById(R.id.bn_login);
+        SignIn = (SignInButton)findViewById(R.id.bn_login);
         Name = (TextView)findViewById(R.id.name);
         Email = (TextView)findViewById(R.id.email);
         Prof_Pic = (ImageView)findViewById(R.id.prof_pic);
         SignIn.setOnClickListener(this);
         SignOut.setOnClickListener(this);
-        
+        Prof_Section.setVisibility(View.GONE);
 
     }
-    protected void onStart() {
-
-        super.onStart();
-    }
-
     @Override
     public void onClick(View view) {
 
