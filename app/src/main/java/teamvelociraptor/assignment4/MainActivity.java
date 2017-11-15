fede189608fc8112450382f096b53ad5c6bb0426
Preston <psegura1@mail.sfsu.edu>
2017-11-15 12:16:46 -0800

    Updated sign in page

--- app/src/main/java/teamvelociraptor/assignment4/MainActivity.java
+++ app/src/main/java/teamvelociraptor/assignment4/MainActivity.java
@@ -1,18 +1,58 @@
 package teamvelociraptor.assignment4;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener{
+
+    private LinearLayout Prof_Section;
+    private Button SignOut;
+    private Button SignIn;
+    private TextView Name,Email;
+    private ImageView Prof_Pic;
+    private GoogleApiClient googleApiClient;
+    private static final int REQ_CODE = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Prof_Section = (LinearLayout)findViewById(R.id.prof_section);
+        SignOut = (Button)findViewById(R.id.bn_logout);
+        SignIn = (Button)findViewById(R.id.bn_login);
+        Name = (TextView)findViewById(R.id.name);
+        Email = (TextView)findViewById(R.id.email);
+        Prof_Pic = (ImageView)findViewById(R.id.prof_pic);
+        SignIn.setOnClickListener(this);
+        SignOut.setOnClickListener(this);
+        
+
     }
     protected void onStart() {
 
         super.onStart();
     }
 
+    @Override
+    public void onClick(View view) {
+
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
