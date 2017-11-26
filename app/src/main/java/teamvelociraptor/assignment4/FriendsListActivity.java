d59e3c5fb583620e448a04c0debe878b12a495a8
Eric Groom <bcgroom@gmail.com>
2017-11-25 22:02:52 -0800

    working database connection.

--- app/src/main/java/teamvelociraptor/assignment4/FriendsListActivity.java
+++ app/src/main/java/teamvelociraptor/assignment4/FriendsListActivity.java
@@ -8,6 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class FriendsListActivity extends AppCompatActivity {
     private static TextView debugText;
 
@@ -19,6 +25,27 @@ public class FriendsListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         debugText = findViewById(R.id.debug);
         debugText.setText("test");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+                // myRef.setValue("Hello World!");
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String value = dataSnapshot.getValue(String.class);
+                        debugText.setText(value);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        debugText.setText("errror: " + databaseError.getMessage());
+                    }
+                });
+            }
+        }).start();
+
     }
 
 }
