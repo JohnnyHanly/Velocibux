1b1d3b45dd58300fa347af790073cb08835d40db
Ryan Stillwagon <stillwagonryan@gmail.com>
2017-12-06 20:44:17 -0800

    Added drop-down navigation window

--- app/src/main/java/teamvelociraptor/assignment4/MainActivity.java
+++ app/src/main/java/teamvelociraptor/assignment4/MainActivity.java
@@ -3,7 +3,10 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuAdapter;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,8 +29,6 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener AuthListener;
     private GoogleApiClient GoogleApiClient;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,34 +37,25 @@ public class MainActivity extends AppCompatActivity {
         Auth = FirebaseAuth.getInstance();
         User = Auth.getCurrentUser();
 
-        launchMessagingActivity();
     }
-    /*this method launches the Messaging Activty after the user presses a button on the main activity.
-I did not know what our plans are for the startup screen so I decided to create a different activty for
-messaging. This is a temporary way to launch messaging until we know how we are actually
-going to stucture the UI
-*/
-    private void launchMessagingActivity(){
-        //button that switches to MessagingActivity
-        Button bmessageSwitchButton = (Button) findViewById(R.id.switcher);
-       bmessageSwitchButton.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               //creates a popup that confirms button press
-               Toast.makeText(MainActivity.this, "Clicked Launch Messaging", Toast.LENGTH_SHORT)
-                       .show();
-
-
-               //Launch Messanging Activity
-               Intent intent= new Intent(MainActivity.this, MessagingActivity.class);
-                startActivity(intent);
-
-           }
-       });
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
 
-
-
-
+        switch (item.getItemId()){
+            case R.id.activity_main_item:
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            case R.id.activity_messaging_item:
+                startActivity(new Intent(this, MessagingActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
