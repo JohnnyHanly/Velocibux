643e915531bcb364e3a5f4f0e1fc8538c868cee7
preston <theteamvelociraptor@gmail.com>
2017-12-08 19:41:20 -0800

    account balance xml

--- app/src/main/java/teamvelociraptor/assignment4/AccountBalance.java
+++ app/src/main/java/teamvelociraptor/assignment4/AccountBalance.java
@@ -13,17 +13,8 @@ public class AccountBalance extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_balance);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
-        });
-    }
 
 }
