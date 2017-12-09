a2c7d513b7010b39d4d8e020503351e4c615bc86
Eric Groom <bcgroom@gmail.com>
2017-12-08 16:40:39 -0800

    basic authentication

--- app/src/main/java/teamvelociraptor/assignment4/MainActivity.java
+++ app/src/main/java/teamvelociraptor/assignment4/MainActivity.java
@@ -1,21 +1,22 @@
 package teamvelociraptor.assignment4;
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,16 +26,30 @@ public class MainActivity extends AppCompatActivity {
     private static DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference();
     private FirebaseAuth.AuthStateListener AuthListener;
     private GoogleApiClient GoogleApiClient;
+    private static final int RC_SIGN_IN = 123; // Needed for auth
+
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
+                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()
+                );
+
+// Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Auth = FirebaseAuth.getInstance();
-        User = Auth.getCurrentUser();
 
         launchMessagingActivity();
         findViewById(R.id.friends).setOnClickListener(new View.OnClickListener() {
@@ -70,6 +85,25 @@ going to stucture the UI
        });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == ResultCodes.OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            } else {
+                // Sign in failed, check response for error code
+                // ... TODO
+            }
+        }
+    }
+
 
 
 
